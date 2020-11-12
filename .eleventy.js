aa9a139373539e1c011949d31229dfb3532f3e02
FilipSzutkowski <filip99.ski@gmail.com>
2020-11-12 21:47:07 +0100

    Basic folder structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addLayoutAlias('base', 'base.njk');
+
   return {
     markdownTemplateEngine: 'njk',
+    dir: {
+      layouts: '_layouts',
+    },
   };
 };
---
+++ index.md
@@ -0,0 +1,7 @@
+---
+layout: base
+---
+
+# Mosgren
+
+Hello there
