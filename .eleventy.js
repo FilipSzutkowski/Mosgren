b7ab31bb65ba772106024931a4f564091756e2fa
FilipSzutkowski <filip99.ski@gmail.com>
2020-12-02 20:44:48 +0100

    working on time display

--- .eleventy.js
+++ .eleventy.js
@@ -17,7 +17,7 @@ module.exports = (eleventyConfig) => {
       minute: 'numeric',
       hour12: false,
     };
-    return new Intl.DateTimeFormat('default', options).format(dateObject);
+    return new Intl.DateTimeFormat('local', options).format(dateObject);
   });
 
   return {
